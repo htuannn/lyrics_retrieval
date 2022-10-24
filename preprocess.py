24e37abb923ca93db07876c682ad813d6dced373
hoangtuankg021 <hoangtuanvnkg021@gmail.com>
2022-10-24 19:41:33 +0700

    #negation handling

--- preprocess.py
+++ preprocess.py
@@ -7,11 +7,13 @@ import string
 import pandas as pd
 import numpy as np
 
+from nltk.corpus import wordnet
+
 class Preprocessing:
 	def __init__(self):
 		self.CONTRACTION_MAP= self._load('dictionary')
 		self.stop_words= set(nltk.corpus.stopwords.words('english'))
-
+		self.stop_words.remove("not")
 	def _load(self, path):
 		#load dictionary file from 'dictionary' folder
 		if 'contraction_word_dictionary.txt' not in os.listdir(path):
@@ -24,7 +26,7 @@ class Preprocessing:
 	def text_lowercase(self, text):
 		return text.lower()
 
-	def covert_unicode(self,text):
+	def convert_unicode(self,text):
 		return text.encode('ascii', 'ignore').decode()
 
 	def delete_tag(self, text):
@@ -34,9 +36,10 @@ class Preprocessing:
 		return  " ".join(text.split())
 
 	def remove_stopwords(self, text):
+
 		word_tokens = nltk.tokenize.word_tokenize(text)
 		filtered_text = [word for word in word_tokens if word not in self.stop_words]
-		return filtered_text
+		return " ".join(filtered_text)
 
 	def remove_punctuation(self, text):
 		translator = str.maketrans('', '', string.punctuation)
@@ -48,15 +51,56 @@ class Preprocessing:
 		"""
 		return ' '.join([self.CONTRACTION_MAP.get(item, item) for item in text.split()])
 
+	def negation_handler(self, sentence):
+		temp = int(0)
+		sentence=nltk.word_tokenize(sentence)
+		for i in range(len(sentence)):
+			if sentence[i-1] in ['not',"n't"]:
+				antonyms = []
+				for syn in wordnet.synsets(sentence[i]):
+					syns = wordnet.synsets(sentence[i])
+					w1 = syns[0].name()
+					temp = 0
+					for l in syn.lemmas():
+						if l.antonyms():
+							antonyms.append(l.antonyms()[0].name())
+					max_dissimilarity = 0
+					for ant in antonyms:
+						syns = wordnet.synsets(ant)
+						w2 = syns[0].name()
+						syns = wordnet.synsets(sentence[i])
+						w1 = syns[0].name()
+						word1 = wordnet.synset(w1)
+						word2 = wordnet.synset(w2)
+						if isinstance(word1.wup_similarity(word2), float) or isinstance(word1.wup_similarity(word2), int):
+							temp = 1 - word1.wup_similarity(word2)
+						if temp>max_dissimilarity:
+							max_dissimilarity = temp
+							antonym_max = ant
+							sentence[i] = antonym_max
+							sentence[i-1] = ''
+		while '' in sentence:
+			sentence.remove('')
+		return sentence
 
-	def Preprocess(self, str):
+	def handle_negation(self, text):
+		match = re.search(r'\b(?:not)\b (\S+)', text)
+		if match:
+			text= text.replace(match.group(0), 'NEG_' + match.group(1))
+		return text
+
+	def Preprocess(self, str, handle_negation= False):
 		str = self.text_lowercase(str)
-		str= self.covert_unicode(str)
+		str= self.convert_unicode(str)
 		str = self.delete_tag(str)
 		str = str.replace('\n\n', '')
 		str = str.replace('\n', ' ')
 		str = self.replace_cw(str)
-		str= self.remove_stopwords(str)
-		str= self.remove_punctuation(" ".join(str))
+		str= self.remove_punctuation(str)
 		str= self.remove_whitespace(str)
+		str= self.remove_stopwords(str)
+		if handle_negation:
+			str = self.negation_handler(str)
+		
+
 		return str
